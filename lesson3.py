f07c6f369abda0d1d6a766fac7819554b3bf4fe2
desswell <kam@hitekki.ru>
2019-01-25 20:05:56 +0300

    у

--- lesson3.py
+++ lesson3.py
@@ -1,21 +1,36 @@
-
 a = input().split('.')
 c = int(input())
 b = a[1][:c]
+u = a[0][:c]
 d = ''
-if c > len(a[1]):
-    b += (len(a[1]) - 2)*'0'
-    d = ''
-elif c < len(a[1]):
+if a[0] == '0':
     x = list(a[1])
     g = int(x[c])
     if g >= 5:
-        x[c-1] = str(int(x[c-1]) + 1)
-    b = ''.join(x)[:c]
-elif c == len(a[1]):
-    a[1] += '0'
-if c != 0:
-    d = a[0] + '.' + b
+        print(1)
+    else:
+        print(0)
 else:
-    d = a[0]
-print(d)
+    d = ''
+    if c > len(a[1]):
+        b += (c - len(a[1]))*'0'
+    elif c < len(a[1]):
+        x = list(a[1])
+        g = int(x[c])
+        y = list(a[0])
+        if g >= 5:
+            if c == 0:
+                u = 1
+                o = 1
+            else:
+                x[c-1] = str(int(x[c-1]) + 1)
+                b = ''.join(x)[:c]
+    elif c == len(a[1]):
+         a[1] += '0'
+    if c != 0:
+        d = a[0] + '.' + b
+    elif c == 0 and o != 1:
+        d = a[0]
+    elif o == 1:
+        d = int(a[0]) + u
+    print(d)
